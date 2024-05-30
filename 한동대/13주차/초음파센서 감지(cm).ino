2ed6206b8431de1f5a69744dd8382c255ed0b4a6
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-05-30 17:06:32 +0900

    Update 초음파센서 감지(cm).ino

--- 한동대/13주차/초음파센서 감지(cm).ino
+++ 한동대/13주차/초음파센서 감지(cm).ino
@@ -1,1 +1,23 @@
+int echoPin = 12;
+int trigPin = 13;
 
+void setup() {
+	Serial.begin(9600);
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+}
+void loop() {
+	float duration, distance;
+	
+	digitalWrite(trigPin, HIGH);
+	delay(10);
+	digitalWrite(trigPin, LOW);
+	
+	duration = pulseIn(echoPin, HIGH); 
+	distance = duration*34000/1000000/2;/*아두이노 초음파 센서 속도? 340m/s  거리를 구하려면 속력*시간이니까 */
+	
+
+	Serial.print(distance);
+	Serial.println("cm");
+	delay(500);
+}
